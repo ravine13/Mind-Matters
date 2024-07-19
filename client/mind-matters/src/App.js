6b8a922f190a80e03b7c9e6dcce6720de41c7a66
ravine13 <kuriaderek@gmail.com>
2024-07-19 19:29:21 +0300

    new: frontend displaying

--- client/mind-matters/src/App.js
+++ client/mind-matters/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Layout from "./layouts/Layout";
 import UserProvider from './context/UserContext';
+import './index.css'; 
 
 export default function App() {
   return (
-    <UserProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </Router>
-    </UserProvider>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </UserProvider>
   );
 }
