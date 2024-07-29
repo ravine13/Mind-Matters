c27472522586dcf4b0928e7f8eb1a6a6a21495f8
ravine13 <kuriaderek@gmail.com>
2024-07-29 13:34:34 +0300

    new; new requiremnts.txt

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager 
 from flask_bcrypt import Bcrypt  
 import os
+from dotenv import load_dotenv 
 from models import db
 from Auth import auth_bp
 from routes.users import user_bp
@@ -16,16 +17,19 @@ from routes.Appointment import appointment_bp
 jwt = JWTManager()
 bcrypt = Bcrypt()
 
+load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
-    app.config['SECRET_KEY'] = b"\x06F\x14\x91\xba\xdc\x9a\x96g'\xc7\xb0"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') 
     
     db.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
     migrate = Migrate(app, db)
+    
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
