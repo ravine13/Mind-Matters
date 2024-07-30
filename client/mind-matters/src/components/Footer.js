136a00cbd2e55c63d2dcb27c4f3307b81e51f0a1
ravine13 <kuriaderek@gmail.com>
2024-07-30 19:29:33 +0300

    new: footer changed

--- client/mind-matters/src/components/Footer.js
+++ client/mind-matters/src/components/Footer.js
@@ -1,15 +1,40 @@
-import { Footer } from 'flowbite-react'
+import { Footer } from 'flowbite-react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaGithub, FaBrain } from 'react-icons/fa';
 
 export default function Foot() {
   return (
     <Footer className="shadow-none px-2 mx-auto w-full max-w-[1280px] rounded-none">
-        <Footer.Copyright href="#" by="Mind Matters™" year={2024} />
-        <Footer.LinkGroup>
-        <Footer.Link href="#">About</Footer.Link>
-        <Footer.Link href="#">Privacy Policy</Footer.Link>
-        <Footer.Link href="#">Terms of Service</Footer.Link>
-        <Footer.Link href="#">Contact</Footer.Link>
-        </Footer.LinkGroup>
+      <div className="text-center py-6">
+        <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900">
+          <FaBrain className="h-12 mr-3 sm:h-9" aria-hidden="true" />
+          Mind-Matters
+        </a>
+        <span className="block text-sm text-center text-gray-500">
+          © 2024 Mind-Matters™. All Rights Reserved. 
+        </span>
+        <ul className="flex justify-center mt-5 space-x-5">
+          <li>
+            <a href="#" className="text-gray-500 hover:text-gray-900">
+              <FaFacebookF className="w-5 h-5" aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-500 hover:text-gray-900">
+              <FaTwitter className="w-5 h-5" aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-500 hover:text-gray-900">
+              <FaInstagram className="w-5 h-5" aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-500 hover:text-gray-900">
+              <FaGithub className="w-5 h-5" aria-hidden="true" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </Footer>
-  ) 
+  );
 }
