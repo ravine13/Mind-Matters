d1b87921b75d3d1b9f2a210a4bc737a0f91c05ce
ravine13 <kuriaderek@gmail.com>
2024-08-03 13:24:50 +0300

    new: footer adjusted

--- client/mind-matters/src/components/Footer.js
+++ client/mind-matters/src/components/Footer.js
@@ -4,15 +4,15 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaGithub, FaBrain } from 'react-ic
 export default function Foot() {
   return (
     <Footer className="shadow-none px-2 mx-auto w-full max-w-[1280px] rounded-none">
-      <div className="text-center py-2"> {/* Reduced the padding here */}
-        <a href="#" className="flex items-center justify-center mb-3 text-2xl font-semibold text-gray-900"> {/* Reduced margin-bottom */}
+      <div className="text-center py-2"> 
+        <a href="#" className="flex items-center justify-center mb-3 text-2xl font-semibold text-gray-900"> 
           <FaBrain className="h-12 mr-3 sm:h-9" aria-hidden="true" />
           Mind-Matters
         </a>
         <span className="block text-sm text-center text-gray-500">
           © 2024 Mind-Matters™. All Rights Reserved. 
         </span>
-        <ul className="flex justify-center mt-3 space-x-4"> {/* Reduced margin-top and space between icons */}
+        <ul className="flex justify-center mt-3 space-x-4"> 
           <li>
             <a href="#" className="text-gray-500 hover:text-gray-900">
               <FaFacebookF className="w-5 h-5" aria-hidden="true" />
