9b23451df640b2898960c2423d2c30dcf8550397
ravine <kuriaderek@gmail.com>
2025-08-04 15:39:27 +0300

    User added in database

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager 
-from flask_bcrypt import Bcrypt  
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
+
 from models import db
 from Auth import auth_bp
 from routes.users import user_bp
@@ -13,34 +14,33 @@ from serializer import serializer_bp
 from routes.contactform import contact_form_bp
 from routes.Appointment import appointment_bp
 
-
 jwt = JWTManager()
 bcrypt = Bcrypt()
-
+ma = Marshmallow()
 load_dotenv()
 
-
 def create_app():
-    app = Flask(__name__)    
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    # app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') 
+    app = Flask(__name__)
+
+    # Use SQLite for development
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
     app.config['SECRET_KEY'] = b"\x06F\x14\x91\xba\xdc\x9a\x96g'\xc7\xb0"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    
     db.init_app(app)
+    ma.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
     migrate = Migrate(app, db)
-    
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(serializer_bp)
     app.register_blueprint(appointment_bp)
     app.register_blueprint(contact_form_bp)
-   
+
     CORS(app, resources={r"*": {"origins": "*"}})
+
     return app
 
 app = create_app()
