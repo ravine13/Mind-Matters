843a8bb61049c98cda8540f9aeacb484c16765ac
ravine <kuriaderek@gmail.com>
2025-08-06 11:20:49 +0300

    env changes

--- server/app.py
+++ server/app.py
@@ -23,8 +23,8 @@ def create_app():
     app = Flask(__name__)
 
     # Use SQLite for development
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
-    app.config['SECRET_KEY'] = b"\x06F\x14\x91\xba\xdc\x9a\x96g'\xc7\xb0"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
